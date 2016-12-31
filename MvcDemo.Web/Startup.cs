f8a26c64eb342842c22211932ba1a5b61f88a61a
Dick Harper <dick.harper@gmail.com>
2016-12-31 05:17:04 -0700

    Add project files.

---
+++ MvcDemo.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvcDemo.Web.Startup))]
+namespace MvcDemo.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
